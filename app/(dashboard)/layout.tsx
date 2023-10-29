669b707e89bd2dc7ab375c1bfcc36848e4d76290
Kelvinn Ranjethdra Dev A/l Devathasan <manvanoss@gmail.com>
2023-10-29 23:59:10 +0800

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -15,9 +15,7 @@ const DashboardLayout = async({
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0  bg-gray-900">
-                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount}>
-
-                </Sidebar>
+                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount}/>
             </div>
             <main className="md:pl-72">
                 <Navbar />
